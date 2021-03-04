e5beb2d644e36bf66ada6268b2a34a9d58b037ab
Itza Reyes <61748503+itzareyes@users.noreply.github.com>
2021-03-03 18:29:26 -0600

    Use Kotlin instead java

---
+++ src/main/kotlin/com/course/springbootbasics/SpringBootBasicsApplication.kt
@@ -0,0 +1,11 @@
+package com.course.springbootbasics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootBasicsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootBasicsApplication>(*args)
+}
