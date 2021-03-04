70747e3751a74eba5303312812ed6524b459a509
Itza Reyes <61748503+itzareyes@users.noreply.github.com>
2021-03-03 19:20:25 -0600

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.jetbrains.kotlin.plugin.jpa") version "1.3.71"
 	kotlin("jvm") version "1.4.30"
 	kotlin("plugin.spring") version "1.4.30"
 }
@@ -15,10 +16,22 @@ repositories {
 	mavenCentral()
 }
 
+val springBootVersion = "2.2.6.RELEASE"
+
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	//Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	//Web
+	implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+
+	// Data
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
+	implementation("org.springframework.data:spring-data-jpa:$springBootVersion")
+	implementation("org.postgresql:postgresql:42.2.12.jre7")
+	implementation("org.flywaydb:flyway-core:6.0.8")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
