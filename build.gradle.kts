08db78196265b52dc107a8dbfd864252d4b49705
Itza Reyes <61748503+itzareyes@users.noreply.github.com>
2021-03-24 18:21:03 -0600

    Fix build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.3"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.3.71"
-	kotlin("plugin.spring") version "1.3.71"
+  id("org.springframework.boot") version "2.4.3"
+  id("io.spring.dependency-management") version "1.0.11.RELEASE"
+  id("org.jetbrains.kotlin.plugin.jpa") version "1.3.71"
+  kotlin("jvm") version "1.4.30"
+  kotlin("plugin.spring") version "1.4.30"
 	id("io.gitlab.arturbosch.detekt") version "1.0.1"
 }
 
